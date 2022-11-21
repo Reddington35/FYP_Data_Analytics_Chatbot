01ece8399deccf193b61cd64e4b0dbf9219fceb2
James Reddington <reddingtonjamesr@gmail.com>
2022-11-21 18:16:24 +0000

    test_commit

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-import pandas as pd
+import pandas as pd,spacy
 from sklearn.ensemble import RandomForestClassifier
+
+spacy.prefer_gpu()
+nlp = spacy.load("en_core_web_sm")
+
+# testing for nlp
+sentence = ('hello how are you')
+test_doc = nlp(sentence)
+print([token.text for token in test_doc])
 
 training_doc = "vac_rates_2021.csv"
 
@@ -17,8 +25,6 @@ print(y_training)
 
 hyper_peram_criterion = "gini"
 hyper_peram_max_depth = 20
-
-training_accuracy = []
 
 model = RandomForestClassifier(criterion = hyper_peram_criterion,max_depth = hyper_peram_max_depth)
 
