f885a394be03b8bd5964716eb8944d8afdc3793f
James Reddington <reddingtonjamesr@gmail.com>
2023-03-08 16:33:11 +0000

    chatbot_tasks_changes_ML_Graph

--- Tasks.py
+++ Tasks.py
@@ -4,6 +4,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVR
 from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.preprocessing import MinMaxScaler
 from sklearn import metrics
 import NLP
 
@@ -27,7 +28,7 @@ def data_plot(dataset, username, request):
     df = pd.read_csv(dataset,index_col=False)
 
 def data_ml(dataset, username, request):
-    df = pd.read_csv(dataset['Location'], sep=',')
+    df = pd.read_csv(dataset['Location'],index_col=False)
     labels = printChoices(df)
 
     print("Colin: Give target feature please " + username)
@@ -39,17 +40,20 @@ def data_ml(dataset, username, request):
     labels = printChoices(df)
     print("Colin: Give parameters please " + username)
     choice = input(username)
+    # print("Colin: Give Hyper - Parameters please " + username)
+    # hp_choice = input(username)
     params = NLP.label_match(choice, labels)
-
     labels = []
 
     for label in params:
         labels.append(label)
 
-    globals()[request['def']](target, labels, df)
+    globals()[request['def']](target, labels, df, username)
 
 # ML Methods
-def RandomForest(target, labels, dataset):
+def RandomForest(target, labels, dataset,username):
+    refinement = True
+    print("Random Forest Classification Algorithm Selected")
     print(dataset)
     newLabels = labels
     newLabels.append(target)
@@ -66,16 +70,119 @@ def RandomForest(target, labels, dataset):
     print(X)
     print(y)
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+    # use min_max scaler to normalise and scale the data
+    scaler = MinMaxScaler(feature_range=(0, 1))
 
-    clf = RandomForestClassifier(n_estimators=10, max_depth=6, criterion="entropy")
-    print("Fitting dataset")
-    clf.fit(X_train, y_train)
-    print("predicting dataset")
-    y_pred = clf.predict(X_test)
-    print("Accuracy:", metrics.accuracy_score(y_test, y_pred))
-    
+    print(scaler.fit(X, y))
+    model = scaler.fit((X))
+    print("Max is")
+    print(model.data_max_)
+    print("min is")
+    print(model.data_min_)
+    scaled_X = model.transform(X)
+    print(scaled_X)
+
+    crit_list = ["gini","entropy"]
+    cl_weight = ["balanced","balanced_subsample"]
+    mx_features = ["sqrt","log2"]
+
+    # Hyper - Parameter Default values
+    user_n_estimators = 100
+    user_max_depth = 2
+    user_criterion = "gini"
+    user_min_samples_split = 2
+    user_min_samples_leaf = 1
+    user_min_weight_fraction_leaf = 0.0
+    user_max_features = "sqrt"
+    user_max_leaf_nodes = None
+    user_min_impurity_decrease = 0.0
+    user_bootstrap = True
+    user_oob_score = False
+    user_n_jobs = None
+    user_random_state = None
+    user_verbose = 0
+    user_warm_start = False
+    user_class_weight = None
+    user_ccp_alpha = 0.0
+    user_max_samples = None
+
+    X_train, X_test, y_train, y_test = train_test_split(scaled_X, y, test_size=0.3)
+    while refinement:
+        clf = RandomForestClassifier(n_estimators=user_n_estimators,max_depth=user_max_depth,criterion=user_criterion,
+                                     min_samples_split=user_min_samples_split,min_samples_leaf=user_min_samples_leaf,
+                                     min_weight_fraction_leaf=user_min_weight_fraction_leaf,max_features=user_max_features,
+                                     max_leaf_nodes=user_max_leaf_nodes,min_impurity_decrease=user_min_impurity_decrease,
+                                     bootstrap=user_bootstrap,oob_score=user_oob_score,n_jobs=user_n_jobs,
+                                     random_state=user_random_state,verbose=user_verbose,warm_start=user_warm_start,
+                                     class_weight=user_class_weight,ccp_alpha=user_ccp_alpha,max_samples=user_max_samples)
+        print("Fitting dataset")
+        clf.fit(X_train, y_train)
+        print("predicting dataset")
+        y_pred = clf.predict(X_test)
+        print("Accuracy:", metrics.accuracy_score(y_test, y_pred))
+        print("Recall:", metrics.recall_score(y_test, y_pred, average='micro'))
+        plt.figure("Training Data")
+        plt.scatter(y_test,y_pred)
+        plt.title("nothing")
+        plt.xlabel("Test Data")
+        plt.ylabel("Predicted Data")
+        plt.show()
+
+        refine_command = input("Colin: Would you like to refine hyper-perameters ?" +"\n" + username)
+        if refine_command.lower().strip() in ['quit', 'no', 'n']:
+            refinement = False
+        else:
+            redifined_list = refine_command.split(",")
+            for redifined in redifined_list:
+                if "n_estimators" in redifined:
+                    user_n_estimators = change_hyperperams_int("n_estimators=", redifined,user_n_estimators)
+                elif "max_depth" in redifined:
+                    user_max_depth = change_hyperperams_int("max_depth=", redifined,user_max_depth)
+                elif "criterion" in redifined:
+                    for crit in crit_list:
+                        if crit in redifined:
+                            user_criterion = change_hyperperams_String("criterion=",redifined,user_criterion)
+                elif "min_samples_split" in redifined:
+                    user_min_samples_split = change_hyperperams_int("min_samples_split=",redifined,user_min_samples_split)
+                elif "min_samples_leaf" in redifined:
+                    user_min_samples_leaf = change_hyperperams_int("min_samples_leaf=",redifined,user_min_samples_leaf)
+                elif "min_weight_fraction_leaf" in redifined:
+                    user_min_weight_fraction_leaf = change_hyperperams_Float("min_weight_fraction_leaf=",redifined,
+                                                                             user_min_weight_fraction_leaf)
+                elif "max_features" in redifined:
+                    for mxf in mx_features:
+                        if mxf in redifined:
+                            user_max_features = change_hyperperams_String("max_features=",redifined,user_max_features)
+                elif "max_leaf_nodes" in redifined:
+                    user_max_leaf_nodes = change_hyperperams_int("max_leaf_nodes=",redifined,user_max_leaf_nodes)
+                elif "min_impurity_decrease" in redifined:
+                    user_min_impurity_decrease = change_hyperperams_Float("min_impurity_decrease=",redifined,
+                                                                          user_min_impurity_decrease)
+                elif "bootstrap" in redifined:
+                    user_bootstrap = change_hyperperams_bool("bootstrap=",redifined,user_bootstrap)
+                elif "oob_score" in redifined:
+                    user_oob_score = change_hyperperams_bool("oob_score=",redifined,user_oob_score)
+                elif "n_jobs" in redifined:
+                    user_n_jobs = change_hyperperams_int("n_jobs=",redifined,user_n_jobs)
+                elif "random_state" in redifined:
+                    user_random_state = change_hyperperams_int("random_state=",redifined,user_random_state)
+                elif "verbose" in redifined:
+                    user_verbose = change_hyperperams_int("verbose=",redifined,user_verbose)
+                elif "warm_start" in redifined:
+                    user_warm_start = change_hyperperams_bool("warm_start=",redifined,user_warm_start)
+                elif "class_weight" in redifined:
+                    for weight in cl_weight:
+                        if weight in redifined:
+                            user_class_weight = change_hyperperams_String("class_weight=",redifined,user_class_weight)
+                elif "ccp_alpha" in redifined:
+                    user_ccp_alpha = change_hyperperams_String("ccp_alpha=",redifined,user_ccp_alpha)
+                elif "max_samples" in redifined:
+                    user_max_samples = change_hyperperams_Float("max_samples=",redifined,user_max_samples)
+                elif "help" in redifined:
+                    helper()
+
 def NaiveBayes(target, labels, dataset):
+    print("Naive Bayes Classification Algorithm Selected")
     print(dataset)
     newLabels = labels
     newLabels.append(target)
@@ -91,6 +198,18 @@ def NaiveBayes(target, labels, dataset):
     y = cleanDataset[target]
     print(X)
     print(y)
+
+    # use min_max scaler to normalise and scale the data
+    scaler = MinMaxScaler(feature_range=(0, 1))
+
+    print(scaler.fit(X, y))
+    model = scaler.fit((X))
+    print("Max is")
+    print(model.data_max_)
+    print("min is")
+    print(model.data_min_)
+    scaled_X = model.transform(X)
+    print(scaled_X)
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
 
@@ -102,6 +221,7 @@ def NaiveBayes(target, labels, dataset):
     print("Accuracy:", metrics.accuracy_score(y_test, y_pred))
 
 def SupportVectorMachine(target, labels, dataset):
+    print("SVM Classification Algorithm Selected")
     print(dataset)
     newLabels = labels
     newLabels.append(target)
@@ -117,6 +237,18 @@ def SupportVectorMachine(target, labels, dataset):
     y = cleanDataset[target]
     print(X)
     print(y)
+
+    # use min_max scaler to normalise and scale the data
+    scaler = MinMaxScaler(feature_range=(0, 1))
+
+    print(scaler.fit(X, y))
+    model = scaler.fit((X))
+    print("Max is")
+    print(model.data_max_)
+    print("min is")
+    print(model.data_min_)
+    scaled_X = model.transform(X)
+    print(scaled_X)
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
 
@@ -217,3 +349,51 @@ def find_target(user_input, dataset):
     else:
         print("not found")
 
+def change_hyperperams_int(hyper_peram,refine_command,Default_value):
+    result = Default_value
+    if hyper_peram in refine_command:
+        if "=" in refine_command:
+            result =  int(refine_command[refine_command.rfind("=") + 1:])
+        else:
+            result = int(refine_command[refine_command.rfind(" ") + 1:])
+    print("Colin: Changed "+ hyper_peram.replace('=','').strip() + " to " + str(result))
+    return result
+
+def change_hyperperams_String(hyper_peram,refine_command,Default_value):
+    result = Default_value
+    if hyper_peram in refine_command:
+        if "=" in refine_command:
+            result = refine_command[refine_command.rfind("=") + 1:]
+        else:
+            result = refine_command[refine_command.rfind(" ") + 1:]
+    print("Colin: Changed " + hyper_peram.replace('=','').strip() + " to " + result)
+    return result
+
+def change_hyperperams_Float(hyper_peram,refine_command,Default_value):
+    result = Default_value
+    if hyper_peram in refine_command:
+        if "=" in refine_command:
+            result = float(refine_command[refine_command.rfind("=") + 1:])
+        else:
+            result = float(refine_command[refine_command.rfind(" ") + 1:])
+    print("Colin: Changed " + hyper_peram.replace('=','').strip() + " to " + str(result))
+    return result
+
+def change_hyperperams_bool(hyper_peram,refine_command,Default_value):
+    commandResult = Default_value
+    if hyper_peram in refine_command:
+        if "=" in refine_command:
+            commandResult = refine_command[refine_command.rfind("=") + 1:].lower() in ['true']
+
+        else:
+            commandResult = refine_command[refine_command.rfind(" ") + 1:].lower() in ['true']
+
+    print("Colin: Changed " + hyper_peram.replace('=','').strip() + " to " + str(commandResult))
+    return commandResult
+
+def helper():
+    print("- Criterion takes the values 'gini' or 'entropy' for example  type: criterion=gini"
+          ", as the command to change this hyper-parameters\n- n_estimators takes an int value, "
+          "for example type: n_estimators=10"
+          ", as the command to change this hyper-parameters\n- max_depth takes an int value for example  type: criterion=gini"
+          ", as the command to change this hyper-parameters\n")
--- practice.py
+++ practice.py
@@ -1,22 +1,27 @@
 import spacy as sp
 from spacy.matcher import PhraseMatcher
+import sklearn
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn import metrics
+from sklearn.preprocessing import MinMaxScaler
 import pandas as pd
 
 nlp = sp.load("en_core_web_lg")
 
-matcher = PhraseMatcher(nlp.vocab)
-matcher.add("Ireland", [nlp("ireland"),nlp("irish")])
-matcher.add("EU", [nlp("arack Obama"),nlp("Baracko")])
-matcher.add("World", [nlp("Scatter plot"), nlp("scatterplot"),nlp("scat")])
-matcher.add("Who",[nlp("Scatter plot"), nlp("scatterplot"),nlp("scat")])
-doc = nlp("Ireland is in ireland which is irish")
-matches = matcher(doc)
-matchesS = [nlp.vocab.strings[s[0]] for s in matches]
-#print(nlp.vocab.strings)
-#print(matchesS)
-target = "Ireland"
-if target in matchesS:
-    print(target," Found!")
+# matcher = PhraseMatcher(nlp.vocab)
+# matcher.add("Ireland", [nlp("ireland"),nlp("irish")])
+# matcher.add("EU", [nlp("arack Obama"),nlp("Baracko")])
+# matcher.add("World", [nlp("Scatter plot"), nlp("scatterplot"),nlp("scat")])
+# matcher.add("Who",[nlp("Scatter plot"), nlp("scatterplot"),nlp("scat")])
+# doc = nlp("Ireland is in ireland which is irish")
+# matches = matcher(doc)
+# matchesS = [nlp.vocab.strings[s[0]] for s in matches]
+# #print(nlp.vocab.strings)
+# #print(matchesS)
+# target = "Ireland"
+# if target in matchesS:
+#     print(target," Found!")
 #print(matches)
 
 # #initilize the matcher with a shared vocab
@@ -37,6 +42,41 @@ if target in matchesS:
 
 
 
-doc = nlp("color pink black blue")
-for token in doc:
-    print(token.dep_)
+# doc = nlp("color pink black blue")
+# # for token in doc:
+# #     print(token.dep_)
+
+
+# ML Methods
+def RandomForest(target, labels, dataset):
+    print(dataset)
+    newLabels = labels
+    newLabels.append(target)
+    print("Labels: " + str(newLabels))
+    newDataset = dataset[newLabels].copy()
+    print("Dataset: ")
+    print(newDataset)
+    cleanDataset = newDataset.dropna()
+    # cleanDataset = cleanDataset.head(50000)
+    print(cleanDataset)
+
+    X = cleanDataset[labels]
+    y = cleanDataset[target]
+    print(X)
+    print(y)
+    # use min_max scaler to normalise and scale the data
+    scaler = MinMaxScaler()
+    print(scaler.fit(X,y))
+    MinMaxScaler()
+    print(scaler.data_max_)
+    print(scaler.transform(X))
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+
+    clf = RandomForestClassifier(n_estimators=10, max_depth=6, criterion="entropy")
+    print("Fitting dataset")
+    clf.fit(X_train, y_train)
+    print("predicting dataset")
+    y_pred = clf.predict(X_test)
+    print("Accuracy:", metrics.accuracy_score(y_test, y_pred))
+
+RandomForest("Cases - cumulative total","Deaths - newly reported in last 24 hours","datasets/WHO_covid.csv")
